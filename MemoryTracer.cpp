6b095c0e0de65a0959711235f5487d53b2ae0c65
Naoya Maruyama <nmaruyama@riken.jp>
2014-08-02 14:04:10 +0900

    Trace call and return

--- MemoryTracer.cpp
+++ MemoryTracer.cpp
@@ -10,6 +10,9 @@
 #include <fstream>
 #include <stdlib.h>
 #include <stddef.h>
+#include <assert.h>
+#include <vector>
+#include <algorithm>
 
 /* ================================================================== */
 // Global variables 
@@ -34,6 +37,8 @@ TLS_KEY mlog_key;
 
 std::ostream * out = &cerr;
 
+std::vector<ADDRINT> instrumented;
+
 /* ===================================================================== */
 // Command line switches
 /* ===================================================================== */
@@ -44,6 +49,7 @@ KNOB<string> KnobFunction(KNOB_MODE_WRITEONCE,  "pintool",
                           "f", "__NO_SUCH_FUNCTION__", "specify the name of function to trace");
 
 string target_func("__NO_SUCH_FUNCTION__");
+ADDRINT target_func_addr = 0;
 
 /* ===================================================================== */
 // Utilities
@@ -54,8 +60,8 @@ string target_func("__NO_SUCH_FUNCTION__");
  */
 INT32 Usage()
 {
-  cerr << "This tool prints out the number of dynamically executed " << endl <<
-      "instructions, basic blocks and threads in the application." << endl << endl;
+  cerr << "This tool records memory reads and stores in the running application."
+       << endl << endl;
 
   cerr << KNOB_BASE::StringKnobSummary() << endl;
 
@@ -78,22 +84,6 @@ struct MEMREF {
   UINT32 type;
   UINT32 size;
 };
-
-#if 0
-// Print a memory read record
-VOID RecordMemRead(VOID * ip, VOID * addr, UINT32 size)
-{
-  *out << ip << ": Reading " << size
-       << " bytes of memory at " << addr << endl;  
-}
-
-// Print a memory write record
-VOID RecordMemWrite(void *ip, VOID * addr, UINT32 size)
-{
-  *out << ip << ": Writing " << size
-       << " bytes of memory at " << addr << endl;  
-}
-#endif
 
 /*
  * MLOG - thread specific data that is not handled by the buffering API.
@@ -208,12 +198,35 @@ VOID Trace(TRACE trace, VOID *v)
 }
 #endif
 
+static int FunctionMatch(ADDRINT target) {
+  return target == target_func_addr;
+}
+
+static void TraceCall(TRACE trace, VOID *v) {
+  for(BBL bbl = TRACE_BblHead(trace); BBL_Valid(bbl); bbl=BBL_Next(bbl)) {
+    for(INS ins = BBL_InsHead(bbl); INS_Valid(ins); ins=INS_Next(ins)) {
+      if (INS_IsProcedureCall(ins)) {
+        INS_InsertIfCall(ins, IPOINT_BEFORE, (AFUNPTR)FunctionMatch,
+                         IARG_BRANCH_TARGET_ADDR, IARG_END);
+        INS_InsertFillBufferThen(
+            ins, IPOINT_BEFORE, bufId,
+            IARG_BRANCH_TARGET_ADDR, offsetof(struct MEMREF, addr),
+            IARG_UINT32, 0, offsetof(struct MEMREF, size),      
+            IARG_UINT32, TRACE_FUNC_BEGIN, offsetof(struct MEMREF, type),      
+            IARG_END);
+      }
+    }
+  }
+}
+
 /*
  * Insert code to write data to a thread-specific buffer for instructions
  * that access memory.
  */
 VOID Trace(TRACE trace, VOID *v)
 {
+  TraceCall(trace, v);
+  
   RTN rtn = TRACE_Rtn(trace);
   if (!RTN_Valid(rtn)) return;
   
@@ -224,21 +237,34 @@ VOID Trace(TRACE trace, VOID *v)
   if (rtn_name != target_func) return;
   
   cerr << "Trace of target function, " << target_func << ", found.\n";
+
+#if 0 // Does not work; why?
+  RTN_Open(rtn);
+  INS ins_head = RTN_InsHeadOnly(rtn);
+  if (std::find(instrumented.begin(), instrumented.end(), INS_Address(ins_head)) ==
+      instrumented.end()) {
+    cerr << "Instrumenting the beginning of function\n";
+    INS_InsertFillBuffer(
+        ins_head, IPOINT_BEFORE, bufId,
+        IARG_ADDRINT, RTN_Address(rtn), offsetof(struct MEMREF, addr),
+        IARG_UINT32, 0, offsetof(struct MEMREF, size),      
+        IARG_UINT32, TRACE_FUNC_BEGIN, offsetof(struct MEMREF, type),      
+        IARG_END);
+    instrumented.push_back(INS_Address(ins_head));
+  }
+  RTN_Close(rtn);
+#endif  
   
-  for(BBL bbl = TRACE_BblHead(trace); BBL_Valid(bbl); bbl=BBL_Next(bbl))
-  {
-    for(INS ins = BBL_InsHead(bbl); INS_Valid(ins); ins=INS_Next(ins))
-    {
+  for(BBL bbl = TRACE_BblHead(trace); BBL_Valid(bbl); bbl=BBL_Next(bbl)) {
+    for(INS ins = BBL_InsHead(bbl); INS_Valid(ins); ins=INS_Next(ins)) {
       UINT32 memoryOperands = INS_MemoryOperandCount(ins);
 
-      for (UINT32 memOp = 0; memOp < memoryOperands; memOp++)
-      {
+      for (UINT32 memOp = 0; memOp < memoryOperands; memOp++) {
         UINT32 refSize = INS_MemoryOperandSize(ins, memOp);
 
         // Note that if the operand is both read and written we log it once
         // for each.
-        if (INS_MemoryOperandIsRead(ins, memOp))
-        {
+        if (INS_MemoryOperandIsRead(ins, memOp)) {
           INS_InsertFillBufferPredicated(
               ins, IPOINT_BEFORE, bufId,
               IARG_MEMORYOP_EA, memOp, offsetof(struct MEMREF, addr),
@@ -247,8 +273,7 @@ VOID Trace(TRACE trace, VOID *v)
               IARG_END);
         }
 
-        if (INS_MemoryOperandIsWritten(ins, memOp))
-        {
+        if (INS_MemoryOperandIsWritten(ins, memOp)) {
           INS_InsertFillBufferPredicated
               (ins, IPOINT_BEFORE, bufId,
                IARG_MEMORYOP_EA, memOp, offsetof(struct MEMREF, addr),
@@ -257,9 +282,56 @@ VOID Trace(TRACE trace, VOID *v)
                IARG_END);
         }
       }
-    }
+
+      if (INS_IsRet(ins)) {
+        INS_InsertFillBuffer
+            (ins, IPOINT_BEFORE, bufId,
+             IARG_ADDRINT, RTN_Address(rtn), offsetof(struct MEMREF, addr),
+             IARG_UINT32, 0, offsetof(struct MEMREF, size), 
+             IARG_UINT32, TRACE_FUNC_END, offsetof(struct MEMREF, type),
+             IARG_END);
+      }
+    }    
   }
 }
+
+// Pin calls this function every time a new rtn is executed
+VOID Routine(RTN rtn, VOID *v)
+{
+  const string &rtn_name = RTN_Name(rtn);
+
+  if (rtn_name != target_func) return;
+  cerr << "Routine of target function, " << target_func << ", found.\n";
+
+  RTN_Open(rtn);
+  ADDRINT rtn_addr = RTN_Address(rtn);
+  cerr << "rtn: " << rtn_addr << endl;
+  target_func_addr = rtn_addr;
+
+#if 0  
+  INS_InsertFillBuffer(
+      ins_head, IPOINT_BEFORE, bufId,
+      IARG_ADDRINT, rtn_addr, offsetof(struct MEMREF, addr),
+      IARG_UINT32, 0, offsetof(struct MEMREF, size),      
+      IARG_UINT32, TRACE_FUNC_BEGIN, offsetof(struct MEMREF, type),      
+      IARG_END);
+
+  
+  // Examine each instruction in the routine.
+  for( INS ins = RTN_InsHead(rtn); INS_Valid(ins); ins = INS_Next(ins) ) {
+    if( INS_IsRet(ins) ) {
+      INS_InsertFillBuffer(
+          ins, IPOINT_BEFORE, bufId,
+          IARG_ADDRINT, rtn_addr, offsetof(struct MEMREF, addr),
+          IARG_UINT32, 0, offsetof(struct MEMREF, size),      
+          IARG_UINT32, TRACE_FUNC_END, offsetof(struct MEMREF, type),      
+          IARG_END);
+    }
+  }
+#endif
+  RTN_Close(rtn);  
+}
+
 
 /**************************************************************************
  *
@@ -372,9 +444,12 @@ int main(int argc, char *argv[])
   // Initialize thread-specific data not handled by buffering api.
   mlog_key = PIN_CreateThreadDataKey(0);
    
+  // Register Routine to be called to instrument rtn
+RTN_AddInstrumentFunction(Routine, 0);
 
   // Register function to be called to instrument traces
   TRACE_AddInstrumentFunction(Trace, 0);
+
 
   // Register function to be called for every thread before it starts running
   PIN_AddThreadStartFunction(ThreadStart, 0);
